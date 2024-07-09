f0f557b301786f55063e8e51546721defcb21b4a
Anthony Cruz <anthonyleierlw@gmail.com>
2024-07-09 14:40:03 -0300

    Estruturação de entidade/tabela participant

---
+++ src/main/java/br/com/anthonycruz/planner/participant/Participant.java
@@ -0,0 +1,35 @@
+package br.com.anthonycruz.planner.participant;
+
+import br.com.anthonycruz.planner.trip.Trip;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "participants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private boolean isConfirmed;
+
+    @ManyToOne
+    @JoinColumn(name = "trip_id", nullable = false)
+    private Trip trip;
+}
