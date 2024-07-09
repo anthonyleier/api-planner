c6e23975fac6dddc31c26e24b1ba717549952fd8
Anthony Cruz <anthonyleierlw@gmail.com>
2024-07-09 09:07:43 -0300

    Initial commit

---
+++ src/test/java/br/com/anthonycruz/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anthonycruz.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
