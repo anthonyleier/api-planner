cbda3e8806af92862eea5a84ee6516869a2514d1
Anthony Cruz <anthonyleierlw@gmail.com>
2024-07-09 14:50:24 -0300

    Endpoint para confirmação de participante

---
+++ src/main/java/br/com/anthonycruz/planner/participant/ParticipantRepository.java
@@ -0,0 +1,8 @@
+package br.com.anthonycruz.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+}
---
+++ src/main/java/br/com/anthonycruz/planner/participant/ParticipantRequestPayload.java
@@ -0,0 +1,4 @@
+package br.com.anthonycruz.planner.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+}
