45838dd2b5858765a9d8c308024c668d1e9d9941
Anthony Cruz <anthonyleierlw@gmail.com>
2024-07-09 09:48:33 -0300

    Estruturação de entidade/tabela trip

---
+++ src/main/java/br/com/anthonycruz/planner/trip/Trip.java
@@ -0,0 +1,31 @@
+package br.com.anthonycruz.planner.trip;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+public class Trip {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String destination;
+
+    @Column(name = "starts_at", nullable = false)
+    private LocalDateTime startsAt;
+
+    @Column(name = "ends_at", nullable = false)
+    private LocalDateTime endsAt;
+
+    @Column(name = "is_confirmed", nullable = false)
+    private LocalDateTime isConfirmed;
+
+    @Column(name = "owner_name", nullable = false)
+    private LocalDateTime ownerName;
+
+    @Column(name = "owner_email", nullable = false)
+    private LocalDateTime ownerEmail;
+}
