8fd6595fbcccad0a13488ffef95f22910997bffe
Anthony Cruz <anthonyleierlw@gmail.com>
2024-08-01 15:01:13 -0300

    Separa os endpoints em confirmar e atualizar participante

--- src/main/java/br/com/anthonycruz/planner/controllers/ParticipantController.java
+++ src/main/java/br/com/anthonycruz/planner/controllers/ParticipantController.java
@@ -4,8 +4,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,15 +25,32 @@ public class ParticipantController {
         this.repository = repository;
     }
 
-    @PostMapping("/{id}/confirm")
-    public ResponseEntity<ParticipantDTO> confirmParticipant(@PathVariable UUID id, @RequestBody ParticipantRequest request) {
+    @PutMapping("/{id}")
+    public ResponseEntity<ParticipantDTO> updateParticipant(@PathVariable UUID id, @RequestBody ParticipantRequest request) {
         Optional<Participant> optionalParticipant = this.repository.findById(id);
 
         if (optionalParticipant.isPresent()) {
             Participant participant = optionalParticipant.get();
 
-            participant.setConfirmed(true);
             participant.setName(request.name());
+            participant.setEmail(request.email());
+
+            Participant savedParticipant = this.repository.save(participant);
+            ParticipantDTO participantDTO = new ParticipantDTO(savedParticipant.getId(), savedParticipant.getName(), savedParticipant.getEmail(), savedParticipant.isConfirmed());
+
+            return ResponseEntity.ok(participantDTO);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/{id}/confirm")
+    public ResponseEntity<ParticipantDTO> confirmParticipant(@PathVariable UUID id) {
+        Optional<Participant> optionalParticipant = this.repository.findById(id);
+
+        if (optionalParticipant.isPresent()) {
+            Participant participant = optionalParticipant.get();
+            participant.setConfirmed(true);
 
             Participant savedParticipant = this.repository.save(participant);
             ParticipantDTO participantDTO = new ParticipantDTO(savedParticipant.getId(), savedParticipant.getName(), savedParticipant.getEmail(), savedParticipant.isConfirmed());
