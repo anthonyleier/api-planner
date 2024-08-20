800257d4491c19019971469b629a0beef56722a1
Anthony Cruz <anthonyleierlw@gmail.com>
2024-08-20 15:32:55 -0300

    frontend: Listagem de atividades com dados da API

--- frontend/src/pages/trip-details/activities.tsx
+++ frontend/src/pages/trip-details/activities.tsx
@@ -1,35 +1,36 @@
 import { CircleCheck } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
+export interface Activity {
+  id: string;
+  title: string;
+  occursAt: string;
+}
 
 export function Activities() {
+  const { tripID } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    api.get(`http://localhost/trips/${tripID}/activities`).then((response) => setActivities(response.data));
+  }, [tripID]);
+
   return (
     <div className="space-y-8">
       <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-          <span className="text-xs text-zinc-500">Sábado</span>
-        </div>
-        <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</p>
-      </div>
-
-      <div className="space-y-2.5">
-        <div className="flex gap-2 items-baseline">
-          <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-          <span className="text-xs text-zinc-500">Domingo</span>
-        </div>
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl flex items-center gap-3">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
-            <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+        {activities.map((activity) => (
+          <div key={activity.id} className="space-y-2.5">
+            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl flex items-center gap-3">
+              <CircleCheck className="size-5 text-lime-300" />
+              <span className="text-zinc-100">{activity.title}</span>
+              <span className="text-zinc-400 text-sm ml-auto">{format(activity.occursAt, "d 'de' MMMM 'às' HH:mm", { locale: ptBR })}</span>
+            </div>
           </div>
-        </div>
-        <div className="space-y-2.5">
-          <div className="px-4 py-2.5 bg-zinc-900 rounded-xl flex items-center gap-3">
-            <CircleCheck className="size-5 text-lime-300" />
-            <span className="text-zinc-100">Academia em grupo</span>
-            <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
