777ca2566b5d19eb6a9db2555d4b604b57016644
Anthony Cruz <anthonyleierlw@gmail.com>
2024-09-03 16:22:50 -0300

    frontend: Ao clicar na imagem redireciona para ela

--- frontend/src/pages/trip-details/photos.tsx
+++ frontend/src/pages/trip-details/photos.tsx
@@ -6,6 +6,7 @@ import { AddPhotoModal } from "./add-photo-modal";
 interface Photo {
   id: string;
   filename: string;
+  url?: string;
 }
 
 export function Photos() {
@@ -40,11 +41,18 @@ export function Photos() {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-        {photos.map((photo) => (
-          <div key={photo.id} className="w-full h-48 overflow-hidden">
-            <img src={`http://localhost:8080/trips/${tripID}/photos/${photo.filename}`} className="w-full h-full object-cover rounded-lg" />
-          </div>
-        ))}
+        {photos.map((photo) => {
+          photo.url = `http://localhost:8080/trips/${tripID}/photos/${photo.filename}`;
+          return (
+            <div key={photo.id} className="relative w-full h-48 overflow-hidden group">
+              <a href={photo.url} target="_blank">
+                <img src={photo.url} className="w-full h-full object-cover rounded-lg transition duration-300 ease-in-out group-hover:opacity-50" alt={`Photo ${photo.id}`} />
+
+                <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-30 rounded-lg"></div>
+              </a>
+            </div>
+          );
+        })}
         <button onClick={openAddPhotoModal} className="flex items-center justify-center w-full h-48 bg-zinc-800 hover:bg-zinc-700 cursor-pointer rounded-lg">
           <span className="text-4xl text-zinc-300">+</span>
         </button>
