eef16d89423e0704b12c81e856571770d236d70d
Anthony Cruz <anthonyleierlw@gmail.com>
2024-09-03 08:31:25 -0300

    frontend: Melhoria na componentização das atividades

--- frontend/src/pages/trip-details/activities.tsx
+++ frontend/src/pages/trip-details/activities.tsx
@@ -1,6 +1,11 @@
-import { CircleCheck } from "lucide-react";
+import { CircleCheck, Plus } from "lucide-react";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
+import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { useCallback, useEffect, useState } from "react";
+import { CreateActivityModal } from "./create-activity-modal";
 
 interface Activity {
   id: string;
@@ -8,24 +13,55 @@ interface Activity {
   occursAt: string;
 }
 
-interface ActivitiesProps {
-  activities: Activity[];
-}
+export function Activities() {
+  const { tripID } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false);
 
-export function Activities({ activities }: ActivitiesProps) {
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true);
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false);
+  }
+
+  const fetchActivities = useCallback(() => {
+    api.get(`/trips/${tripID}/activities`).then((response) => setActivities(response.data));
+  }, [tripID]);
+
+  function handleActivityCreated() {
+    fetchActivities();
+  }
+
+  useEffect(() => {
+    fetchActivities();
+  }, [fetchActivities, tripID]);
+
   return (
-    <div className="space-y-8">
-      <div className="space-y-2.5">
-        {activities.map((activity) => (
-          <div key={activity.id} className="space-y-2.5">
-            <div className="px-4 py-2.5 bg-zinc-900 rounded-xl flex items-center gap-3">
-              <CircleCheck className="size-5 text-lime-300" />
-              <span className="text-zinc-100">{activity.title}</span>
-              <span className="text-zinc-400 text-sm ml-auto">{format(activity.occursAt, "d 'de' MMMM 'às' HH:mm", { locale: ptBR })}</span>
-            </div>
-          </div>
-        ))}
+    <>
+      <div className="flex items-center justify-between">
+        <h2 className="text-3xl font-semibold">Atividades</h2>
+        <Button onClick={openCreateActivityModal} variant="primary">
+          <Plus className="size-5" />
+          Cadastrar atividade
+        </Button>
       </div>
-    </div>
+      <div className="space-y-8">
+        <div className="space-y-2.5">
+          {activities.map((activity) => (
+            <div key={activity.id} className="space-y-2.5">
+              <div className="px-4 py-2.5 bg-zinc-900 rounded-xl flex items-center gap-3">
+                <CircleCheck className="size-5 text-lime-300" />
+                <span className="text-zinc-100">{activity.title}</span>
+                <span className="text-zinc-400 text-sm ml-auto">{format(activity.occursAt, "d 'de' MMMM 'às' HH:mm", { locale: ptBR })}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {isCreateActivityModalOpen && <CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} onActivityCreated={handleActivityCreated} />}
+    </>
   );
 }
--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -1,47 +1,11 @@
-import { Plus } from "lucide-react";
-import { useCallback, useEffect, useState } from "react";
-import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
 import { Guests } from "./guests";
 import { Activities } from "./activities";
 import { DestinationAndDateHeader } from "./destination-and-date-header";
-import { Button } from "../../components/button";
 import { TripStatus } from "./trip-status";
-import { useParams } from "react-router-dom";
-import { api } from "../../lib/axios";
 import { Photos } from "./photos";
 
-interface Activity {
-  id: string;
-  title: string;
-  occursAt: string;
-}
-
 export function TripDetailsPage() {
-  const { tripID } = useParams();
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] = useState(false);
-
-  function openCreateActivityModal() {
-    setIsCreateActivityModalOpen(true);
-  }
-
-  function closeCreateActivityModal() {
-    setIsCreateActivityModalOpen(false);
-  }
-
-  const fetchActivities = useCallback(() => {
-    api.get(`/trips/${tripID}/activities`).then((response) => setActivities(response.data));
-  }, [tripID]);
-
-  function handleActivityCreated() {
-    fetchActivities();
-  }
-
-  useEffect(() => {
-    fetchActivities();
-  }, [fetchActivities, tripID]);
-
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <DestinationAndDateHeader />
@@ -49,14 +13,7 @@ export function TripDetailsPage() {
       <main className="flex gap-16 px-4">
         {/* Left */}
         <div className="flex-1 space-y-6">
-          <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-semibold">Atividades</h2>
-            <Button onClick={openCreateActivityModal} variant="primary">
-              <Plus className="size-5" />
-              Cadastrar atividade
-            </Button>
-          </div>
-          <Activities activities={activities} />
+          <Activities />
           <Photos />
         </div>
 
@@ -69,8 +26,6 @@ export function TripDetailsPage() {
           <Guests />
         </div>
       </main>
-
-      {isCreateActivityModalOpen && <CreateActivityModal closeCreateActivityModal={closeCreateActivityModal} onActivityCreated={handleActivityCreated} />}
     </div>
   );
 }
