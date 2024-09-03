83bc344103cc636ebb74c43fcb8b6237f5d22a94
Anthony Cruz <anthonyleierlw@gmail.com>
2024-09-03 10:51:29 -0300

    Aumenta o tamanho da coluna do filename

--- src/main/resources/db/migration/V10__create_table_photo.sql
+++ src/main/resources/db/migration/V10__create_table_photo.sql
@@ -1,6 +1,6 @@
 CREATE TABLE photo (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    filename VARCHAR(20) NOT NULL,
+    filename VARCHAR(255) NOT NULL,
     trip_id UUID,
     FOREIGN KEY (trip_id) REFERENCES trip(id) ON DELETE CASCADE
 );
