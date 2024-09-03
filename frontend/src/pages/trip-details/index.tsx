a78bee4bdba9603a3c63f7f2ed66932c13a217d9
Anthony Cruz <anthonyleierlw@gmail.com>
2024-09-03 08:21:32 -0300

    frontend: Componentização do grid de fotos

--- frontend/src/pages/trip-details/index.tsx
+++ frontend/src/pages/trip-details/index.tsx
@@ -9,6 +9,7 @@ import { Button } from "../../components/button";
 import { TripStatus } from "./trip-status";
 import { useParams } from "react-router-dom";
 import { api } from "../../lib/axios";
+import { Photos } from "./photos";
 
 interface Activity {
   id: string;
@@ -56,23 +57,7 @@ export function TripDetailsPage() {
             </Button>
           </div>
           <Activities activities={activities} />
-
-          <div className="flex items-center justify-between">
-            <h2 className="text-3xl font-semibold">Fotos</h2>
-          </div>
-
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <img src="https://via.placeholder.com/150" className="w-full h-auto object-cover" />
-            <div className="flex items-center justify-center w-full h-auto bg-gray-200 cursor-pointer">
-              <span className="text-4xl text-gray-500">+</span>
-            </div>
-          </div>
+          <Photos />
         </div>
 
         {/* Right */}
